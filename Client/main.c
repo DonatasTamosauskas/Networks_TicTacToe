48b80b0a61feda90e911a31326fddb77bdd4cd0a
Donatas <donatas.tamosauskas@gmail.com>
2019-03-09 18:26:53 +0200

    Implemented move playing in Client

--- Client/main.c
+++ Client/main.c
@@ -12,11 +12,13 @@
 #define PORT "9034"
 #define SERVER_ADDRESS "localhost"
 #define DEFAULT_ERROR_RETURN -1
+#define DEFAULT_RETURN 0
 #define MAX_HOSTNAME_LENGTH 200
+#define MAX_RETRY_COUNT 10
 
-struct packetData {
-    int enemyMove;
+struct packet_data {
     int gameState;
+    int enemyMove;
     int x;
     int y;
 };
@@ -26,14 +28,18 @@ int getPortNumber(char port[]);
 void handleError(int errorCode, int errorType);
 int connectToPort(struct addrinfo *ai, int *socketFd);
 int getServerAddress(char address[], int address_length);
-int sendData(int socketFd, struct packetData *data, int timeout);
+int sendData(int socketFd, struct packet_data *data, int timeout);
+int receiveData(int socketFd, struct packet_data *data);
+int playMatch(int socketFd);
+int playMove(int socketFd);
+int displayGameBoard(struct packet_data *gameData);
 
 
 int main() {
 
     const char hostPort[5], hostname[MAX_HOSTNAME_LENGTH];
     struct addrinfo hints, *addrInfo;
-    int socketFd;
+    int socketFd, gameRunning;
 
     prepareAddrinfoHints(&hints);
 
@@ -57,25 +63,109 @@ int main() {
     }
 
     freeaddrinfo(addrInfo);
+    gameRunning = 1;
+
+    while(gameRunning) {
+        playMatch(socketFd);
+    }
 
 
+}
+/*
+ * Desc:
+ * Params:
+ *
+ * Returns:
+ */
+int playMatch(int socketFd) {
+    struct packet_data game_data;
+    memset(&game_data, 0, sizeof(struct packet_data));
+
+    //sendData(socketFd, &game_data, MAX_RETRY_COUNT);
+    receiveData(socketFd, &game_data);
+    printf("GameState: %d.\n", game_data.gameState);
+
+    if(game_data.gameState == 0) {
+        printf("Waiting for a second client to connect.\n");
+        return DEFAULT_RETURN;
+
+    } else if(game_data.gameState == 1) {
+        if(game_data.enemyMove == 0) {
+            printf("Your move.\n");
+            displayGameBoard(&game_data);
+            playMove(socketFd);
+
+        } else {
+            printf("Wait for your turn.\n");
+            return DEFAULT_RETURN;
+        }
+    }
+    return DEFAULT_ERROR_RETURN;
+}
+
+/*
+ * Desc:
+ * Params:
+ *
+ * Returns:
+ */
+int playMove(int socketFd) {
+    int x, y;
+    struct packet_data data;
+    memset(&data, 0, sizeof(struct packet_data));
+
+    printf("Please enter coordinates you'd like to play. \n");
+    printf("Enter the X coordinate and then Y.\n");
+    scanf("%d %d", &x, &y);
+
+    if(x >= 1 && x <=3 && y >= 1 && y <= 3) {
+        data.x = x;
+        data.y = y;
+        return sendData(socketFd, &data, MAX_RETRY_COUNT);
+
+    } else {
+        printf("Please enter valid coordinates!\n");
+        return playMove(socketFd);
+    }
+}
+
+/*
+ * Desc:
+ * Params:
+ *
+ * Returns:
+ */
+int displayGameBoard(struct packet_data *gameData) {
+    return 0;
 }
 
 /*
  * Desc: Function repeatedly tries to send all data until it's all sent or a timeout is reached.
  * Params:
  *   socketFd - socket file descriptor to be used for sending data
- *   data - packetData to send
+ *   data - packet_data to send
  *   timeout - number of retries to send data in case it's not sent
  * Returns: 0 if sent, -1 if error
  */
-int sendData(int socketFd, struct packetData *data, int timeout) {
-    int sentBits = send(socketFd, data, sizeof(struct packetData), 0);
+int sendData(int socketFd, struct packet_data *data, int timeout) {
+    int sentBits = send(socketFd, data, sizeof(struct packet_data), 0);
 
-    if(sentBits < sizeof(struct packetData) && timeout > 0) {
-        return (sendData(socketFd, data, timeout) == sizeof(struct packetData)) -1;
+    if(sentBits < sizeof(struct packet_data) && timeout > 0) {
+        return (sendData(socketFd, data, timeout) == sizeof(struct packet_data)) -1;
     }
-    return (sentBits == sizeof(struct packetData)) -1;
+    return (sentBits == sizeof(struct packet_data)) -1;
+}
+
+/*
+ * Desc:
+ * Params:
+ *   socketFd - socket file descriptor to listen for
+ *   data - packet_data buffer to fill with received data
+ * Returns: 0 if sent, -1 if error
+ */
+int receiveData(int socketFd, struct packet_data *data) {
+    int receivedBits = recv(socketFd, data, sizeof(struct packet_data), 0);
+    return (receivedBits == sizeof(struct packet_data)) -1;
 }
 
 
