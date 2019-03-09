d62c4da95977b505d99c44282dc5e8302c7181b8
Donatas <donatas.tamosauskas@gmail.com>
2019-03-09 21:44:40 +0200

    Board coordinate start replaced to 0 instead of 1.

--- Client/main.c
+++ Client/main.c
@@ -118,7 +118,7 @@ int playMove(int socketFd) {
     printf("Enter the X coordinate and then Y.\n");
     scanf("%d %d", &x, &y);
 
-    if(x >= 1 && x <=3 && y >= 1 && y <= 3) {
+    if(x >= 0 && x <=2 && y >= 0 && y <= 2) {
         data.x = x;
         data.y = y;
         return sendData(socketFd, &data, MAX_RETRY_COUNT);
