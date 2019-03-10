282ba13939e101e82442ef5f63742502a9941f2a
Donatas <donatas.tamosauskas@gmail.com>
2019-03-10 15:59:52 +0200

    Implemented replayability and reformatted code.

--- Server/main.c
+++ Server/main.c
@@ -32,6 +32,7 @@
 #define RECEIVING_BUFFER_SIZE 255
 #define MAX_SEND_RETRY_COUNT 10
 #define BOARD_SIZE 3
+#define TIME_BETWEEN_GAMES 1000
 
 #define NEW_CONNECTION 10
 #define NEW_DATA 20
@@ -59,61 +60,79 @@ struct game_state {
 };
 
 void prepareAddrinfoHints(struct addrinfo *info);
+
 void handleError(int errorCode, int errorType);
+
 int getPortNumber(char port[]);
+
 int bindToPort(struct addrinfo *ai, int *listener);
+
 int handleConnections(int listener, fd_set *master, int *maxFd, struct received *data, int *gameRunning);
+
 int handleNewConnection(int listener, fd_set *master, int *maxFd);
+
 int handleExistingConnection(int i, fd_set *master, int *maxFd, struct received *data);
+
 int sendData(int socketFd, struct packet_data *data, int timeout);
-int executeGame(int connection_type, struct received *receivedData, struct game_state *gameState, int gameBoard[BOARD_SIZE][BOARD_SIZE]);
+
+int executeGame(int connection_type, struct received *receivedData, struct game_state *gameState,
+                int gameBoard[BOARD_SIZE][BOARD_SIZE]);
+
 int addPlayer(struct received *receivedData, struct game_state *gameState);
+
 void clearGameBoard(int gameBoard[BOARD_SIZE][BOARD_SIZE]);
+
 int handlePlayerDisconnect(struct game_state *gameState, struct received *receivedData);
+
 int handleGameSequence(struct game_state *gameState, struct received *receivedData, int gameBoard[BOARD_SIZE][BOARD_SIZE]);
+
 int handleNewPlayer(struct game_state *gameState, struct received *receivedData);
+
 int checkIfWon(int gameBoard[BOARD_SIZE][BOARD_SIZE], struct game_state *gameState);
+
+void resetGame(struct game_state *gameState, struct received *receivedData, int gameBoard[BOARD_SIZE][BOARD_SIZE],
+               int *connectionType);
 
 int main() {
 
-    int listener, maxFd;
+    int listener, maxFd, connectionType;
     int gameRunning;
     const char hostPort[5];
     struct addrinfo hints, *addrInfo;
-    struct received received_data;
+    struct received receivedData;
     struct packet_data packetData;
     struct game_state gameState;
     int gameBoard[BOARD_SIZE][BOARD_SIZE];
     fd_set master;
 
     gameRunning = 1;
-    memset(&received_data, 0, sizeof(struct received));
+    memset(&receivedData, 0, sizeof(struct received));
     memset(&packetData, 0, sizeof(struct packet_data));
     memset(&gameState, 0, sizeof(struct game_state));
-    received_data.data = &packetData;
+    receivedData.data = &packetData;
     FD_ZERO(&master);
 
 
     prepareAddrinfoHints(&hints);
 
-    if((getPortNumber(hostPort)) != 0) {
+    if ((getPortNumber(hostPort)) != 0) {
         handleError(errno, 1);
         return DEFAULT_ERROR_RETURN;
     }
 
-    if(getaddrinfo(NULL, hostPort, &hints, &addrInfo) != 0) {
+    if (getaddrinfo(NULL, hostPort, &hints, &addrInfo) != 0) {
         handleError(errno, 2);
         return DEFAULT_ERROR_RETURN;
     }
 
-    if((bindToPort(addrInfo, &listener)) != 0) {
+    if ((bindToPort(addrInfo, &listener)) != 0) {
         handleError(errno, 3);
         return DEFAULT_ERROR_RETURN;
     }
 
     freeaddrinfo(addrInfo);
 
-    if(listen(listener, MAX_CONNECTION_QUEUE) !=0){
+    if (listen(listener, MAX_CONNECTION_QUEUE) != 0) {
         handleError(errno, 4);
         return DEFAULT_ERROR_RETURN;
     }
@@ -121,15 +140,20 @@ int main() {
     FD_SET(listener, &master);
     maxFd = listener;
 
-    while(gameRunning) {
-        int connectionType = handleConnections(listener, &master, &maxFd, &received_data, &gameRunning);
-        executeGame(connectionType, &received_data, &gameState, gameBoard);
-        if(DEBUG) printf("Game state: %d\n", gameState.gameState);
+    while (gameRunning) {
+
+        if (gameState.gameState != 2) {
+            connectionType = handleConnections(listener, &master, &maxFd, &receivedData, &gameRunning);
+        } else {
+            resetGame(&gameState, &receivedData, gameBoard, &connectionType);
+        }
+
+        executeGame(connectionType, &receivedData, &gameState, gameBoard);
+        if (DEBUG) printf("Game state: %d\n", gameState.gameState);
     }
 
     return DEFAULT_RETURN;
 }
-
 
 /*
  * Desc:
@@ -137,21 +161,30 @@ int main() {
  *
  * Returns:
  */
-int executeGame(int connection_type, struct received *receivedData, struct game_state *gameState, int gameBoard[BOARD_SIZE][BOARD_SIZE]) {
-    if(gameState->gameState == 0 && connection_type == NEW_CONNECTION) {
+int executeGame(int connection_type, struct received *receivedData, struct game_state *gameState,
+                int gameBoard[BOARD_SIZE][BOARD_SIZE]) {
+    if (gameState->gameState == 0 && connection_type == NEW_CONNECTION) {
         clearGameBoard(gameBoard);
         return handleNewPlayer(gameState, receivedData);
 
-    } else if(gameState->gameState == 1 && connection_type == NEW_DATA) {
+    } else if (gameState->gameState == 1 && connection_type == NEW_DATA) {
         return handleGameSequence(gameState, receivedData, gameBoard);
 
-    } else if(connection_type == DISCONNECTED) {
+    } else if (connection_type == DISCONNECTED) {
         return handlePlayerDisconnect(gameState, receivedData);
     }
 
-    //TODO: Handle connection type 2 (winning scenario)
-
     return -3;
+}
+
+void resetGame(struct game_state *gameState, struct received *receivedData, int gameBoard[BOARD_SIZE][BOARD_SIZE],
+               int *connectionType) {
+    gameState->gameState = 0;
+    receivedData->fileDescriptor = gameState->client2;
+    gameState->client2 = 0;
+    *connectionType = NEW_CONNECTION;
+    clearGameBoard(gameBoard);
+    sleep(TIME_BETWEEN_GAMES);
 }
 
 /*
@@ -161,7 +194,8 @@ int executeGame(int connection_type, struct received *receivedData, struct game_
  *   receivedData - data received from handleConnections function
  * Returns: -1 on error, 0 otherwise
  */
-int handleGameSequence(struct game_state *gameState, struct received *receivedData, int gameBoard[BOARD_SIZE][BOARD_SIZE]) {
+int
+handleGameSequence(struct game_state *gameState, struct received *receivedData, int gameBoard[BOARD_SIZE][BOARD_SIZE]) {
     int winner;
     struct packet_data data;
     memset(&data, 0, sizeof(struct packet_data));
@@ -177,7 +211,7 @@ int handleGameSequence(struct game_state *gameState, struct received *receivedDa
     }
 
     winner = checkIfWon(gameBoard, gameState);
-    if(winner == gameState->client1) {
+    if (winner == gameState->client1) {
         data.gameState = 2;
         data.enemyMove = 0;
         sendData(gameState->client1, &data, MAX_SEND_RETRY_COUNT);
@@ -187,7 +221,7 @@ int handleGameSequence(struct game_state *gameState, struct received *receivedDa
         gameState->gameState = 2;
         return DEFAULT_RETURN;
 
-    } else if(winner == gameState->client2) {
+    } else if (winner == gameState->client2) {
         data.gameState = 2;
         data.enemyMove = 0;
         sendData(gameState->client2, &data, MAX_SEND_RETRY_COUNT);
@@ -197,7 +231,7 @@ int handleGameSequence(struct game_state *gameState, struct received *receivedDa
         gameState->gameState = 2;
         return DEFAULT_RETURN;
 
-    } else if(winner == -1) {
+    } else if (winner == -1) {
         data.gameState = 2;
         data.enemyMove = 2;
         sendData(gameState->client1, &data, MAX_SEND_RETRY_COUNT);
@@ -205,7 +239,7 @@ int handleGameSequence(struct game_state *gameState, struct received *receivedDa
         gameState->gameState = 2;
         return DEFAULT_RETURN;
 
-    } else if(receivedData->fileDescriptor == gameState->client1) {
+    } else if (receivedData->fileDescriptor == gameState->client1) {
         data.enemyMove = 0;
         sendData(gameState->client2, &data, MAX_SEND_RETRY_COUNT);
 
@@ -213,7 +247,7 @@ int handleGameSequence(struct game_state *gameState, struct received *receivedDa
         sendData(gameState->client1, &data, MAX_SEND_RETRY_COUNT);
         return DEFAULT_RETURN;
 
-    } else if(receivedData->fileDescriptor == gameState->client2) {
+    } else if (receivedData->fileDescriptor == gameState->client2) {
         data.enemyMove = 0;
         sendData(gameState->client1, &data, MAX_SEND_RETRY_COUNT);
 
@@ -239,12 +273,12 @@ int checkIfWon(int gameBoard[BOARD_SIZE][BOARD_SIZE], struct game_state *gameSta
     int client2 = gameState->client2;
 
     // Vertical & draw
-    for(int i = 0; i < BOARD_SIZE; i++) {
+    for (int i = 0; i < BOARD_SIZE; i++) {
         sumClient1 = 0;
         sumClient2 = 0;
         drawSum = 0;
 
-        for(int j = 0; j < BOARD_SIZE; j++) {
+        for (int j = 0; j < BOARD_SIZE; j++) {
             sumClient1 += gameBoard[i][j] == client1;
             sumClient2 += gameBoard[i][j] == client2;
 
@@ -252,48 +286,48 @@ int checkIfWon(int gameBoard[BOARD_SIZE][BOARD_SIZE], struct game_state *gameSta
             drawSum += gameBoard[i][j] == client2;
         }
 
-        if(sumClient1 == BOARD_SIZE) return client1;
-        if(sumClient2 == BOARD_SIZE) return client2;
+        if (sumClient1 == BOARD_SIZE) return client1;
+        if (sumClient2 == BOARD_SIZE) return client2;
 
-        if(drawSum == BOARD_SIZE * BOARD_SIZE) return -1;
+        if (drawSum == BOARD_SIZE * BOARD_SIZE) return -1;
     }
 
     // Horizontal
-    for(int i = 0; i < BOARD_SIZE; i++) {
+    for (int i = 0; i < BOARD_SIZE; i++) {
         sumClient1 = 0;
         sumClient2 = 0;
 
-        for(int j = 0; j < BOARD_SIZE; j++) {
+        for (int j = 0; j < BOARD_SIZE; j++) {
             sumClient1 += gameBoard[j][i] == client1;
             sumClient2 += gameBoard[j][i] == client2;
         }
 
-        if(sumClient1 == BOARD_SIZE) return client1;
-        if(sumClient2 == BOARD_SIZE) return client2;
+        if (sumClient1 == BOARD_SIZE) return client1;
+        if (sumClient2 == BOARD_SIZE) return client2;
     }
 
     // Diagonal
     sumClient1 = 0;
     sumClient2 = 0;
 
-    for(int i = 0; i < BOARD_SIZE; i++) {
+    for (int i = 0; i < BOARD_SIZE; i++) {
         sumClient1 += gameBoard[i][i] == client1;
         sumClient2 += gameBoard[i][i] == client2;
     }
 
-    if(sumClient1 == BOARD_SIZE) return client1;
-    if(sumClient2 == BOARD_SIZE) return client2;
+    if (sumClient1 == BOARD_SIZE) return client1;
+    if (sumClient2 == BOARD_SIZE) return client2;
 
     sumClient1 = 0;
     sumClient2 = 0;
 
-    for(int i = 0; i < BOARD_SIZE; i++) {
+    for (int i = 0; i < BOARD_SIZE; i++) {
         sumClient1 += gameBoard[i][BOARD_SIZE - i] == client1;
         sumClient2 += gameBoard[i][BOARD_SIZE - i] == client2;
     }
 
-    if(sumClient1 == BOARD_SIZE) return client1;
-    if(sumClient2 == BOARD_SIZE) return client2;
+    if (sumClient1 == BOARD_SIZE) return client1;
+    if (sumClient2 == BOARD_SIZE) return client2;
 
     return 0;
 }
@@ -305,7 +339,7 @@ int checkIfWon(int gameBoard[BOARD_SIZE][BOARD_SIZE], struct game_state *gameSta
  *    gameBoard - board to reset
  */
 void clearGameBoard(int gameBoard[BOARD_SIZE][BOARD_SIZE]) {
-    for(int i = 0; i < BOARD_SIZE; i++) {
+    for (int i = 0; i < BOARD_SIZE; i++) {
         memset(gameBoard[i], 0, sizeof(gameBoard[i]));
     }
 }
@@ -321,17 +355,17 @@ void clearGameBoard(int gameBoard[BOARD_SIZE][BOARD_SIZE]) {
  *    -2 on disconnection from non-client
  */
 int handlePlayerDisconnect(struct game_state *gameState, struct received *receivedData) {
-    if(gameState->gameState == 0) {
+    if (gameState->gameState == 0) {
         gameState->client1 = 0;
-        if(DEBUG) printf("Player #1 disconnected. No more connected players.\n");
+        if (DEBUG) printf("Player #1 disconnected. No more connected players.\n");
         return DEFAULT_RETURN;
 
-    } else if(gameState->gameState == 1) {
-        if(gameState->client1 == receivedData->fileDescriptor) {
+    } else if (gameState->gameState == 1) {
+        if (gameState->client1 == receivedData->fileDescriptor) {
             gameState->client1 = gameState->client2;
 
-        } else if(receivedData->fileDescriptor != gameState->client1 &&
-                  receivedData->fileDescriptor != gameState->client2) {
+        } else if (receivedData->fileDescriptor != gameState->client1 &&
+                   receivedData->fileDescriptor != gameState->client2) {
             return DEFAULT_ERROR_RETURN;
         }
 
@@ -341,9 +375,9 @@ int handlePlayerDisconnect(struct game_state *gameState, struct received *receiv
         struct packet_data exportData;
         memset(&exportData, 0, sizeof(struct packet_data));
         exportData.gameState = 0;
-        if(sendData(gameState->client1, &exportData, MAX_SEND_RETRY_COUNT) == -1) return DEFAULT_ERROR_RETURN;
+        if (sendData(gameState->client1, &exportData, MAX_SEND_RETRY_COUNT) == -1) return DEFAULT_ERROR_RETURN;
 
-        if(DEBUG) printf("Player #2 disconnected.\n");
+        if (DEBUG) printf("Player #2 disconnected.\n");
         return DEFAULT_RETURN;
 
     } else {
@@ -363,19 +397,21 @@ int handleNewPlayer(struct game_state *gameState, struct received *receivedData)
     struct packet_data exportData;
     memset(&exportData, 0, sizeof(struct packet_data));
 
-    if(players < 0) {
+    if (players < 0) {
         return DEFAULT_ERROR_RETURN;
 
     } else if (players == 1) {
         exportData.gameState = 0;
-        if(sendData(gameState->client1, &exportData, MAX_SEND_RETRY_COUNT) == -1) return DEFAULT_ERROR_RETURN;
+        if (sendData(gameState->client1, &exportData, MAX_SEND_RETRY_COUNT) == -1) return DEFAULT_ERROR_RETURN;
 
-        if(DEBUG) printf("Player #1 added.\n");
+        if (DEBUG) printf("Player #1 added.\n");
         return DEFAULT_RETURN;
 
     } else if (players == 2) {
         exportData.gameState = 1;
         exportData.enemyMove = 0;
+        exportData.x = -1;
+        exportData.y = -1;
         if (sendData(gameState->client1, &exportData, MAX_SEND_RETRY_COUNT) == -1) return DEFAULT_ERROR_RETURN;
 
         exportData.enemyMove = 1;
@@ -401,11 +437,11 @@ int handleNewPlayer(struct game_state *gameState, struct received *receivedData)
  *    2 - if second client added
  */
 int addPlayer(struct received *receivedData, struct game_state *gameState) {
-    if(gameState->client1 == 0) {
+    if (gameState->client1 == 0) {
         gameState->client1 = receivedData->fileDescriptor;
         return 1;
 
-    } else if(gameState->client2 == 0) {
+    } else if (gameState->client2 == 0) {
         gameState->client2 = receivedData->fileDescriptor;
         return 2;
 
@@ -425,10 +461,10 @@ int addPlayer(struct received *receivedData, struct game_state *gameState) {
 int sendData(int socketFd, struct packet_data *data, int timeout) {
     int sentBits = send(socketFd, data, sizeof(struct packet_data), 0);
 
-    if(sentBits < sizeof(struct packet_data) && timeout > 0) {
-        return (sendData(socketFd, data, timeout) == sizeof(struct packet_data)) -1;
+    if (sentBits < sizeof(struct packet_data) && timeout > 0) {
+        return (sendData(socketFd, data, timeout) == sizeof(struct packet_data)) - 1;
     }
-    return (sentBits == sizeof(struct packet_data)) -1;
+    return (sentBits == sizeof(struct packet_data)) - 1;
 }
 
 /*
@@ -448,16 +484,16 @@ int handleConnections(int listener, fd_set *master, int *maxFd, struct received 
     fd_set readFds = *master;
     int newFd;
 
-    if(select(*maxFd + 1, &readFds, NULL, NULL, NULL) < 0) {
+    if (select(*maxFd + 1, &readFds, NULL, NULL, NULL) < 0) {
         handleError(errno, 5);
         return DEFAULT_ERROR_RETURN;
     }
 
-    for(int i = 0; i <= *maxFd; i++){
-        if(FD_ISSET(i, &readFds)) {
-            if(i == listener) {
+    for (int i = 0; i <= *maxFd; i++) {
+        if (FD_ISSET(i, &readFds)) {
+            if (i == listener) {
 
-                if((newFd = handleNewConnection(listener, master, maxFd)) < 0) {
+                if ((newFd = handleNewConnection(listener, master, maxFd)) < 0) {
                     return DEFAULT_ERROR_RETURN;
 
                 } else {
@@ -468,10 +504,10 @@ int handleConnections(int listener, fd_set *master, int *maxFd, struct received 
 
             } else {
                 int receivedBits = handleExistingConnection(i, master, maxFd, data);
-                if(receivedBits < 0) {
+                if (receivedBits < 0) {
                     return DEFAULT_ERROR_RETURN;
 
-                } else if(receivedBits == 0) {
+                } else if (receivedBits == 0) {
                     return DISCONNECTED;
 
                 } else {
@@ -494,11 +530,11 @@ int handleConnections(int listener, fd_set *master, int *maxFd, struct received 
  *   maxFd - maximum file descriptor currently in use
  *   data - data buffer to be filled with packet data
  */
-int handleExistingConnection(int incomingFd, fd_set *master, int *maxFd, struct received *data){
-    if(DEBUG) printf("Existing connection incoming.\n");
+int handleExistingConnection(int incomingFd, fd_set *master, int *maxFd, struct received *data) {
+    if (DEBUG) printf("Existing connection incoming.\n");
     int recv_bits = recv(incomingFd, data->data, sizeof(struct packet_data), 0);
 
-    if(recv_bits < 0) {
+    if (recv_bits < 0) {
         handleError(errno, 7);
         close(incomingFd);
         FD_CLR(incomingFd, master);
@@ -528,14 +564,14 @@ int handleExistingConnection(int incomingFd, fd_set *master, int *maxFd, struct 
  *   maxFd - the maximum file descriptor currently in use
  * Returns: -1 if error, new file descriptor otherwise.
  */
-int handleNewConnection(int listener, fd_set *master, int *maxFd){
-    if(DEBUG) printf("New connection incoming\n");
+int handleNewConnection(int listener, fd_set *master, int *maxFd) {
+    if (DEBUG) printf("New connection incoming\n");
     struct socaddr_storage *remoteAddress;
     socklen_t addr_size = sizeof(remoteAddress);
 
     int newFd = accept(listener, (struct sockaddr *) remoteAddress, &addr_size);
 
-    if(newFd == -1) {
+    if (newFd == -1) {
         handleError(errno, 6);
         return DEFAULT_ERROR_RETURN;
     } else {
@@ -556,20 +592,20 @@ int bindToPort(struct addrinfo *ai, int *listener) {
 
     struct addrinfo *curr;
 
-    for(curr = ai; curr != NULL; curr = curr->ai_next) {
+    for (curr = ai; curr != NULL; curr = curr->ai_next) {
 
         *listener = socket(curr->ai_family, curr->ai_socktype, curr->ai_protocol);
-        if(*listener < 0) continue;
+        if (*listener < 0) continue;
 
         int bindError = bind(*listener, curr->ai_addr, curr->ai_addrlen);
-        if(bindError < 0) {
+        if (bindError < 0) {
             close(*listener);
             continue;
         }
         break;
     }
 
-    if(curr == NULL) {
+    if (curr == NULL) {
         return -1;
     } else {
         return 0;
@@ -583,9 +619,9 @@ int bindToPort(struct addrinfo *ai, int *listener) {
  */
 void prepareAddrinfoHints(struct addrinfo *info) {
     memset(info, 0, sizeof(struct addrinfo));
-    info -> ai_family = AF_UNSPEC;
-    info -> ai_socktype = SOCK_STREAM;
-    info -> ai_flags = AI_PASSIVE;
+    info->ai_family = AF_UNSPEC;
+    info->ai_socktype = SOCK_STREAM;
+    info->ai_flags = AI_PASSIVE;
 }
 
 /*
@@ -595,7 +631,7 @@ void prepareAddrinfoHints(struct addrinfo *info) {
  * Returns: Error code.
  */
 int getPortNumber(char port[]) {
-    for(int i = 0; i < 5; i++){
+    for (int i = 0; i < 5; i++) {
         port[i] = PORT[i];
     }
     return 0;
@@ -618,7 +654,7 @@ int getPortNumber(char port[]) {
  *
  */
 void handleError(int errorCode, int errorType) {
-    switch(errorType) {
+    switch (errorType) {
         case 1:
             printf("Unable to get port number. Error code: %d\n", errorCode);
             break;
